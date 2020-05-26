7b6c6a660610510c69b96d7adbbe6a08dd2ff2f0
Daniel Olsson <daniel.olsson@spinit.se>
2020-05-26 11:09:26 +0200

    Added summary on CreateDbOptions class

--- Spinit.CosmosDb/Internals/CreateDbOptions.cs
+++ Spinit.CosmosDb/Internals/CreateDbOptions.cs
@@ -2,6 +2,11 @@
 {
     public class CreateDbOptions
     {
+        /// <summary>
+        /// Options for when creating a new database.
+        /// </summary>
+        /// <param name="throughput">The throughput to set. Must be between 400 and 1000000 and in increments of 100.</param>
+        /// <param name="throughputType">Where to set the default throughput. Sets the throughput for all containers if Container is selected.</param>
         public CreateDbOptions(int throughput, ThroughputType throughputType)
         {
             Throughput = throughput;

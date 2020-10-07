102afc390654f6d57b619233199efe5798a89f50
Victor Alveflo <victor.alveflo@spinit.se>
2020-10-07 11:24:26 +0200

    Introduce SpinitCosmosDbException and add error handling for non ok responds

---
+++ Spinit.CosmosDb/SpinitCosmosDbException.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Net;
+
+namespace Spinit.CosmosDb
+{
+    public class SpinitCosmosDbException : Exception
+    {
+        public HttpStatusCode HttpStatusCode { get; private set; }
+
+        public SpinitCosmosDbException(HttpStatusCode httpStatusCode, string message)
+            : base(message)
+        {
+            HttpStatusCode = httpStatusCode;
+        }
+
+        public SpinitCosmosDbException(HttpStatusCode httpStatusCode, string message, Exception innerException)
+            : base(message, innerException)
+        {
+            HttpStatusCode = httpStatusCode;
+        }
+    }
+}
